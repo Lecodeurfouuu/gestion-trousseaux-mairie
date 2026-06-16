1116c53780a73ce66880035c4617150e293c79e1
Lecodeurfouuu <rayanmvpro@gmail.com>
2026-06-16 13:50:46 +0200

    chore: apply pending changes (remove video, update reset_donnees)

--- Information Projet/reset_donnees.sql
+++ Information Projet/reset_donnees.sql
@@ -1,6 +1,3 @@
-
-SET FOREIGN_KEY_CHECKS = 0;
-
 TRUNCATE TABLE element_acces;
 TRUNCATE TABLE trousseau_elements;
 TRUNCATE TABLE historique_trousseaux;
@@ -9,5 +6,4 @@ TRUNCATE TABLE badges;
 TRUNCATE TABLE references_cles;
 TRUNCATE TABLE batiments;
 TRUNCATE TABLE personnes;
-
-SET FOREIGN_KEY_CHECKS = 1;
+TRUNCATE TABLE portes
